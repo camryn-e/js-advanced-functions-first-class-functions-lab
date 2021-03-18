18e010039dbeb1e5caec79068ce6495749cbc8ae
camryn williams <williams.camryn.e@gmail.com>
2021-03-18 14:44:35 -0400

    commit tree 7adddcc17d0f09de4c3a9834de9590de7bc00e53

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    const first = drivers.slice(0,2);
+    return first;
+}
+
+const returnLastTwoDrivers = function(drivers){
+    const last = drivers.slice(-2);
+    return last;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(mult){
+    function fareMult(fare){
+        return fare*mult;
+    }
+    return fareMult;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
